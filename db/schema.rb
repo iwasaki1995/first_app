9b5fd44507fd63b7c51b0ee5a32da788876a9a15
iwasaki1995 <114895033+iwasaki1995@users.noreply.github.com>
2022-10-21 08:47:57 +0000

    [Finish] 2-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_112803) do
+ActiveRecord::Schema.define(version: 2022_10_21_084049) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
